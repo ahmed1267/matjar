d68c6a834975615abac256b35fd1a6c3db9852e1
ahmed-mohamed-eng <ahmed.mohamed.eng05@gmail.com>
2023-11-26 03:36:15 +0200

    fixed the structure of Item

--- src/item/dto/create-item.dto.ts
+++ src/item/dto/create-item.dto.ts
@@ -1,33 +1,20 @@
-import { Prop } from '@nestjs/mongoose';
-import { IsNotEmpty, IsString, IsArray, IsDateString, Validate, MinLength, MaxLength, IsEnum, IsEmail, IsPhoneNumber, Matches } from 'class-validator';
-
 export enum Category {
-    ACCSESSORIES = 'accessories',
-    HOME = 'home',
-    GYM = 'gym',
-    OTHERS= 'others'
+  ACCESSORIES = 'accessories',
+  HOME = 'home',
+  GYM = 'gym',
+  OTHERS = 'others',
 }
 export class CreateItemDto {
-    // Item title, must not be empty, and should be a string
-    @IsNotEmpty({ message: 'An item must have a title' })
-    @IsString({ message: 'An item must have a string title' })
-    name: string;
+  name: string;
+  price: number;
+  amount: number;
+  userID: string;
+  description: string;
+  category: Category[];
 
-    // User password, must not be empty, and should be a valid date string
-    @IsNotEmpty({ message: 'An item must have a description' })
-    @IsString({ message: 'An item description must be string' })
-    @MinLength(20, { message: 'An item description must be 6 chracters minimum' })
-    @MaxLength(150, { message: 'An item description must be 150 chracters maximum' })
-    description: String;
-
-    // User Role, must not be empty and should be of the current options
-    @IsNotEmpty({ message: 'An item must have a category' })
-    @IsEnum(Category, { each: true, message: 'Invalid ready option' })
-    category: Category[];
-
-    @Prop({ required: true })
-    @IsNotEmpty({ message: 'Image is required' })
-    @IsEmail()
-    image: string;
-
+  brand?: string;
+  rating?: number;
+  sizes?: string[];
+  images?: string[];
+  colors?: string[];
 }
