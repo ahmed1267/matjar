6f08f41ea3b2604376962ff258b03bdddd645880
ahmed1267 <ahmedadel837@gmail.com>
2024-01-06 19:03:18 +0200

    Update shop.controller.ts

--- src/shop/shop.controller.ts
+++ src/shop/shop.controller.ts
@@ -16,7 +16,9 @@ import { JwtGuard } from 'src/auth/guards/jwt-auth.guard';
 
 @Controller('shop')
 export class ShopController {
-  constructor(private readonly shopService: ShopService) { }
+  constructor(private readonly shopService: ShopService
+private authService: AuthService,
+) { }
 
   @UseGuards(JwtGuard)
   @Post()
